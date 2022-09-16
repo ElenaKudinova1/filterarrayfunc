319ae7f346dce0ebe75440821d500b7218fe203d
ElenaKudinova1 <cool.e-l-e-n-a2012@yandex.ru>
2022-09-17 02:44:31 +0300

    получение массива через ввод в консоли

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] unFilteredArray = {"1234", "1567", "-2", "computer science"};
+
+static string[] GetFilteredArray(string[] Arr) {
+    Console.WriteLine ("Введите массив: ");
+    string str = Console.ReadLine();
+    string[] arrayFromString = str.Split(' ');
+    Console.WriteLine(arrayFromString);
+    return arrayFromString;
+}
