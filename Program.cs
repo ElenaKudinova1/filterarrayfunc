8e10b5ef3958f1e8b9f2f572d62032ccb946d249
ElenaKudinova1 <cool.e-l-e-n-a2012@yandex.ru>
2022-09-17 03:00:15 +0300

    добавлен вывод в консоль

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿string[] unFilteredArray = {"1234", "1567", "-2", "computer science"};
 
-static string[] GetFilteredArray(string[] Arr) {
+void GetFilteredArray(string[] Arr) {
     Console.WriteLine ("Введите массив: ");
     string str = Console.ReadLine();
     string[] arrayFromString = str.Split(' ');
-    Console.WriteLine(arrayFromString);
     string[] filteredArray = arrayFromString.Where(str => str.Length <= 3).ToArray();
-    return filteredArray;
+    for (int i = 0; i < filteredArray.Length; i ++) {
+        Console.WriteLine(filteredArray[i]);
+    }
 }
+GetFilteredArray(unFilteredArray);
